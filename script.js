c951f81175f134185c7097c2e22df097ac478521
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2024-03-07 22:31:41 +0530

    Add code to test the start game button

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Zero-Cross Game");
+//Gameboard IIFE
+const Gameboard = (function () {
+  let gameboard = [
+    ["", "", "hello YOU"],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const render = function () {
+    let boardSquare = "";
+    gameboard.forEach;
+  };
+})();
+
+const startButton = document.getElementById("start-game");
+startButton.addEventListener("click", function () {
+  //Game.start();
+});
