352359c60eaa881a44336a51f5d0ff70984a5ed9
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2024-03-07 22:52:21 +0530

    Create visual gameboard on pressing the start game button

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ const Gameboard = (function () {
   ];
 
   const render = function () {
-    let boardSquare = "";
-    gameboard.forEach;
+    const body = document.body;
+    const gridContainer = document.getElementById("grid-container");
+
+    for (let rowNumber = 0; rowNumber <= 2; rowNumber++) {
+      for (let colNumber = 0; colNumber <= 2; colNumber++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("row-number", rowNumber);
+        cell.setAttribute("col-number", colNumber);
+        cell.classList.add("cell");
+        gridContainer.appendChild(cell);
+      }
+    }
+    body.appendChild(gridContainer);
+  };
+
+  return {
+    render,
   };
 })();
 
 const startButton = document.getElementById("start-game");
 startButton.addEventListener("click", function () {
-  //Game.start();
+  Gameboard.render();
 });
